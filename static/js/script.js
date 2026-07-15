d05e29b2b6b8f2de07d22076c1a4f1318554f612
Enzo <enzogabrielp86@gmail.com>
2026-07-15 17:58:49 -0300

    fix: ajusta tamanho dos cards

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,9 @@
 const botaoSobre = document.getElementById("sobre");
-const botaoComecar = document.getElementsByClassName("btn-primary");
-
-botaoComecar.addEventListener("click", () => {
-   window.location.href = "chat.html";
-})
-
 
 botaoSobre.addEventListener("click", () => {
 
-    alert(
-`CopaIA
-
-Projeto desenvolvido utilizando Microsoft Foundry.
-
-Este agente responde perguntas relacionadas à Copa do Mundo de 2026.`
+    alert(`CopaIA Projeto desenvolvido utilizando Microsoft Foundry. 
+        Este agente responde perguntas relacionadas à Copa do Mundo de 2026.`
     );
 
 });
